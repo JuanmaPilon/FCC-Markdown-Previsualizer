228496bd026fb7f199fde60cf68a846390ad9b16
unknown <juanmartin.pilon@gmail.com>
2023-05-29 14:04:10 -0300

    Agregue el CSS y algun retoque mas

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import './App.css';
+import Editor from './components/Editor';
+import Preview from './components/Preview';
 
 function App() {
   return (
     <div className="App">
+      <Editor/>
       
     </div>
   );
